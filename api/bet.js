b863114429580c2f84d9d05775c59872bf08f449
Jørgen Nummedal Sveberg <jorgenns@stud.ntnu.no>
2023-05-13 14:51:17 +0200

    fixed late bet checking

--- api/bet.js
+++ api/bet.js
@@ -5,15 +5,18 @@ export default async function handler(req, res) {
     if (req.method === "POST") {
         const key = req.body["username"];
         const value = req.body["country_order"];
-        let closed = await client.sql`SELECT * FROM config WHERE setting = 'closed';`;
-        if (closed) return res.status(403).json("betting has closed");
-        try {
-            await client.sql`INSERT INTO bets (username, country_order) VALUES (${key}, ${value})`;
-            res.status(200).send(JSON.stringify('bet added'));
-        } catch (e) {
-            await client.sql`UPDATE bets SET country_order = ${value} WHERE username = ${key}`;
-            res.status(500).send(JSON.stringify(e));
-        }
+        client.sql`SELECT * FROM config WHERE setting = 'closed';`.then(closed => {
+            if (closed.rows.find(x => x.setting === 'closed')["value"]) return res.status(403).json("betting has closed");
+            try {
+                client.sql`INSERT INTO bets (username, country_order) VALUES (${key}, ${value})`.then(() => {
+                    res.status(200).send(JSON.stringify('bet added'));
+                });
+            } catch (e) {
+                client.sql`UPDATE bets SET country_order = ${value} WHERE username = ${key}`.then(() => {
+                    res.status(500).send(JSON.stringify(e));
+                });
+            }
+        })
     } else if (req.method === "GET") {
         const bet = await client.sql`SELECT * FROM bets WHERE key = ${req.body["username"]};`;
         return res.status(200).json({ bet });
