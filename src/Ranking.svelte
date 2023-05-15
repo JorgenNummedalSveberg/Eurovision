3f81781f345f905a5304ddd19d9202ad32f99aed
Jørgen Nummedal Sveberg <jorgenns@stud.ntnu.no>
2023-05-15 13:55:14 +0200

    added detailed scoring

--- src/Ranking.svelte
+++ src/Ranking.svelte
@@ -16,13 +16,25 @@
     function sortRanking(tempBets, tempScores) {
         tempBets.forEach(bet => {
             bet["score"] = 0;
+            bet["perfect"] = 0;
+            bet["medium"] = 0;
+            bet["low"] = 0;
             bet.country_order.forEach((country, index) => {
                 let scoreIndex = tempScores.findIndex(x => x.code === country.code);
 
                 if (parseInt(tempScores[scoreIndex].details.judScore) + parseInt(tempScores[scoreIndex].details.audScore) === 0) return;
                 let tempScore = 3;
                 tempScore -= Math.abs(index-scoreIndex)
-
+                switch (Math.max(tempScore, 0)) {
+                    case 3:
+                        bet.perfect += 1;
+                        break;
+                    case 2:
+                        bet.medium += 1;
+                        break;
+                    case 1:
+                        bet.low += 1;
+                }
                 bet.score+=Math.max(tempScore, 0);
             })
         })
@@ -34,7 +46,12 @@
 <main>
     <div class="list-labels">
         <div>Username</div>
-        <div>Score</div>
+        <div class="score-right">
+            <div>1pts</div>
+            <div>2pts</div>
+            <div>3pts</div>
+            <div>Score</div>
+        </div>
     </div>
     {#if bets.length === 0}Loading...{/if}
     {#each bets as bet, index}
@@ -47,8 +64,11 @@
                     {bet.username}
                 </div>
             </div>
-           <div class="score">
-               {bet.score}
+           <div class="score-right">
+               <div>{bet.low}</div>
+               <div>{bet.medium}</div>
+               <div>{bet.perfect}</div>
+               <div>{bet.score}</div>
            </div>
         </Slot>
     {/each}
@@ -69,6 +89,16 @@
         display: flex;
         align-items: center;
         padding: 8px;
+    }
+
+    .score-right {
+        width: 200px;
+        height: 100%;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+        align-items: center;
+        background-color: rgba(1, 1, 1, 0.5);
     }
 
     .score {
